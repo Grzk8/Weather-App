e9326094e81b8a58b64f585be6b93a8d3e2de707
Grzegorz Kaczor <grzesiek125@op.pl>
2020-09-08 18:22:14 +0200

    weather_info

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,19 @@ import './App.css';
 import Form from './Form';
 import WeatherInfo from './WeatherInfo'
 
+const time = new Date().toLocaleDateString();
 class App extends Component {
-
   state  = {
     location: '',
     date: '',
     temp: '',
     wind: '',
-    err: ''
+    error: false
   }
 
   handleInputHange = e => {
     this.setState({
-      location : e.target.value
+      location : e.target.value.toUpperCase()
     })
   }
 
@@ -25,15 +25,27 @@ class App extends Component {
       const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.location}&appid=2c401902a18d83e97a3b4870577a0579&units=metric`;
 
       fetch(API)
-      .then(response => {
+      .then(response => { 
         if (response.ok){
           return response
         }
         throw Error ("błąd")
       })
+  
       .then(response => response.json())
-      .then(data => console.log(data))
-      .catch (err => console.log(err))
+      .then(data => 
+        this.setState({
+          error: false,
+          location: this.state.location,
+          date: time,
+          temp: data.main.temp.toFixed(0),
+          wind: data.wind.speed
+        }))
+      .catch (err => console.log(err));
+      this.setState({
+        error: true,
+        location: this.state.location
+      })
   }
 
   render() {
@@ -44,7 +56,7 @@ class App extends Component {
           change={this.handleInputHange}
           submit={this.handleLocationSubmit}
           />
-          <WeatherInfo />
+          <WeatherInfo weather={this.state}/>
       </div>
     );
   }
--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
-const WeatherInfo = () => {
+const WeatherInfo = (props) => {
+
+    const {location, date, temp, wind, error} = props.weather;
+
+    let cont = null;
+
+    if(!error && temp) {
+        cont = (
+            <div>
+                <h1>{location}</h1>
+                <div>{date}</div>
+                <div>temp : {temp}<sup>o</sup>C</div>
+                <div>wind speed: {wind}km/h</div>
+            </div>
+        )
+    }
+
     return(
-        <p>info</p>
+        <div>
+            {error ? `nie ma ${location}` : cont}
+        </div>
     )
 }
 
